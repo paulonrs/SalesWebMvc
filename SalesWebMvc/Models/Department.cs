d506d8ffe036b42336bba36cc2481dfe90e9006c
Paulo <paulonrss@gmail.com>
2022-07-09 03:07:53 -0300

    Fiz merda e tive que fazer um novo repositorio :)

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Seller> Sallers { get; set; } = new List<Seller>();
+
+        public Department() { }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSalles(Seller sl)
+        {
+            Sallers.Add(sl);
+        }
+        public void RemoveSalles(Seller sl)
+        {
+            Sallers.Remove(sl);
+        }
+        public double TotalSales(DateTime initial, DateTime ending)
+        {
+            return Sallers.Sum(seller => seller.TotalSales(initial, ending));
+        }
+    }
+}
---
+++ SalesWebMvc/Models/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models
+{
+    public enum SaleStatus
+    {
+        PENDING = 0,
+        BILLED = 1,
+        CANCELED = 2,
+    }
+}
